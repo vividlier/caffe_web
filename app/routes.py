19af8dfa5e316094b471d4ffeaf566d0fad7d2e6
xiao <2379259142@qq.com>
2017-10-18 17:01:04 +0800

    登录注册

---
+++ app/routes.py
@@ -0,0 +1,76 @@
+
+from flask import Flask, render_template, request, redirect,url_for, session
+import config
+from models import User
+from exts import db
+
+app = Flask(__name__)
+app.config.from_object(config)
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        telephone = request.form.get('telephone')
+        password = request.form.get('password')
+        user = User.query.filter(User.telephone == telephone, User.password == password).first()
+        if user:
+            # g.user = user
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        else:
+            return '手机号或密码错误'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        telephone = request.form.get('telephone')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter(User.telephone == telephone).first()
+        if user:
+            return '改手机号码已被注册'
+        else:
+            if password1 != password2:
+                return '两次密码不相等'
+            else:
+                user = User(
+                    telephone=telephone,
+                    username=username,
+                    password=password1)
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for('login'))
+
+@app.route('/logout/')
+def logout():
+    # session.pop('user_id')
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.route('/question/', methods=['GET', 'POST'])
+def question():
+    if request.method == 'GET':
+        return render_template('question.html')
+    else:
+        pass
+
+@app.context_processor
+def my_context_processor():
+    user_id = session.get('user_id')
+    print(user_id)
+    if user_id:
+        user = User.query.filter(User.id == user_id).first()
+        if user:
+            return {'user': user}
+    return {}
