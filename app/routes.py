6de827d328d32aaf5f0dde39e1806a166db16d97
yyc <15900617310@163.com>
2017-10-19 17:32:18 +0800

    comments

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/register/', methods=['POST'])
 def register():
-    #获取登录页面，用户输入的数据(用户名，密码)
+    #获取注册页面，用户输入的数据(用户名，密码)
     data = request.get_json()
     #将用户名和密码传入model模块(py文件)，生成user实例
     user = User(username=data.get('username'), password=data.get('password'))
@@ -20,14 +20,14 @@ def register():
 @app.route('/login/', methods=['POST'])
 def login():
     data = request.get_json()
-    print(User.query.filter(User.username == data.get('username'),
-                             User.password == data.get('password')))
+    #user代表登录时，根据其用户名和密码获得该条记录的id，first()之前的是一整条SQL语句
     user = User.query.filter(User.username == data.get('username'),
                              User.password == data.get('password')).first()
-    print(user)
+    #若该用户id存在，将user.id存入session
     if user:
         session['user_id'] = user.id
         return 'login'
+    #否则登录错误
     else:
         return 'login error'
 
