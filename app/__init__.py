fe051ddf9bdc9adecea6d18508df8fc5cf695a6d
yyc <15900617310@163.com>
2017-10-19 14:37:25 +0800

    register login logout

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,7 @@ from app import config
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
+app.secret_key=config.secret
 db = SQLAlchemy(app)
 
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,33 @@
-from app import app,db
-from flask import request,session
-from model import *
-
+from flask import Flask, render_template, request, redirect,url_for, session
+from app import app
+from app.model import *
 
 @app.route('/')
 def index():
     return 'index'
 
-@app.route('/register/',methods = ['POST'])
+@app.route('/login/', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter(User.username == data.get('username'),
+                             User.password == data.get('password')).first()
+    if user:
+        session['user_id'] = user.id
+        return 'login'
+    else:
+        return 'login error'
+
+@app.route('/register/', methods=['POST'])
 def register():
     data = request.get_json()
-    password1 = request.form.get('password1')
-    password2 = request.form.ger('password2')
-    user = User(
-        username = data.get('username'),
-        password = data.get('password')
-    )
+    print(data)
+    print(data.get('password'))
+    user = User(username=data.get('username'), password=data.get('password'))
     db.session.add(user)
     db.session.commit()
     return 'register'
 
-@app.route('/login/',methods = ['POST'])
-def login():
-    data = request.get_json()
-    user = User.query.filter(User.username == data.get('username'),User.password == data.get('password')).first()
-    if user:
-        session['user_id'] = user.index
-        return 'login'
-    else:
-        return 'error'
-
 @app.route('/logout/')
 def logout():
     session.clear()
-    return 'logout'  
+    return 'logout'
