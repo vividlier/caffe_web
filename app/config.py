99646a675a08be812138917b0b216ba092c05de6
fhiutc <673898885@qq.com>
2017-10-20 11:07:34 +0800

    model hokan

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+secret = 'aGVsbG8gd29ybGQ='
