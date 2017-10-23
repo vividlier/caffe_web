f39af8ea669304c403f11c4880edf5f1dc2723bc
fhiutc <673898885@qq.com>
2017-10-23 17:33:13 +0800

    注册功能

--- caffe_web/dataset/routes.py
+++ caffe_web/dataset/routes.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, request
-# from caffe_web.model import *
+from caffe_web.model import *
+import  json
 
-blueprint = Blueprint(__name__,__name__)
+blueprint = Blueprint(__name__, __name__)
 
 
 @blueprint.route('/', methods=['POST'])
 def test():
     rst = request.get_data()
-    print rst
-    return rst
+    b = json.loads(rst)
+    # print type(b)
+    return '{} is ok'.format(b.get("username"))
+
+
 
 
 # @blueprint.route('/test1', methods=['POST'])
--- caffe_web/routes.py
+++ caffe_web/routes.py
@@ -1,10 +1,24 @@
 from flask import Blueprint
+from flask import request
+from caffe_web import model
+from caffe_web import db
+import json
 
 blueprint = Blueprint(__name__,__name__)
 print __name__
 
 
-@blueprint.route('/', methods=['POST'])
+@blueprint.route('/signUp', methods=['POST'])
+def signUp():
+    tmp = json.loads(request.get_data())
+    print "username is {}".format(tmp.get("username"))
+    print "password is {}".format(tmp.get("password"))
+    user = model.User(username = tmp.get("username"), password = tmp.get("password"))
+    db.session.add(user)
+    db.session.commit()
+    return 'hello'
+
+@blueprint.route('/signIn', methods=['POST'])
 def fun():
     print '1'
     return '1'
