f9e4122adbcb868c1667c93b02c7908aa1c94aea
fhiutc <673898885@qq.com>
2017-10-31 17:32:23 +0800

    完善注册功能，增加密码hash

--- caffe_web/routes.py
+++ caffe_web/routes.py
@@ -30,32 +30,33 @@ def signUp():
     print user.id
     session['user_id'] = user.id
     # db.session.close()
-    return (jsonify({'username': user.username}), 201,
+    return (jsonify({'user_id': user.id, 'username': user.username}), 201,
             {'Location': url_for('caffe_web.routes.get_user', id = user.id, _external = True)})
 
 
 @auth.verify_password
 def verify_password(username_or_token, password):
     # first try to authenticate by token
-    user = User.verify_auth_token(username_or_token)
-    if not user:
+    # user = User.verify_auth_token(username_or_token)
+    # if not user:
         # try to authenticate with username/password
-        user = User.query.filter_by(username = username_or_token).first()
-        if not user or not user.verify_password(password):
-            return False
+    user = User.query.filter_by(username = username_or_token).first()
+    if not user or not user.verify_password(password):
+        return False
     g.user = user
     return True
 
 
 @blueprint.route('/signIn', methods = ['POST'])
 def signIn():
-    tmp = request.get_json()
-    username = tmp.get("username")
-    password = tmp.get("password")
-    user = User.query.filter(User.username == username, User.password == password).first()
-    if user:
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if verify_password(username, password):
+        user = User.query.filter(User.username == username).first()
         session['user_id'] = user.id
-        return "signIn success"
+        return (jsonify({"status":"200","data":{'user_id': user.id, 'username': user.username}}))
+    else:
+        abort(400)
 
 
 @blueprint.route('/<int:id>')
