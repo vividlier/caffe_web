dd624ac9209ce95f1a0f706a5d4e1f4b1d02fcfc
yyc <15900617310@163.com>
2017-10-31 19:16:00 +0800

    projectList

--- caffe_web/project/routes.py
+++ caffe_web/project/routes.py
@@ -1,9 +1,29 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from caffe_web.model import *
 
 blueprint = Blueprint(__name__, __name__)
 
-@blueprint.route('/getProjectDetailById', methods=["POST"])
+@blueprint.route('/getProjectListByUser', methods = ["POST"])
+def  getProjectListByUser():
+    data = request.get_json()
+    userId = data.get('userId')
+    project = Project.query.filter(Project.user_id == userId).first()
+    d = {
+        "status": "ok",
+        "data":{
+            "project_list":[
+                {
+                    "project_id": project.id,
+                    "project_name": project.project_name,
+                    "updated_time": project.updated_at
+                }
+            ]
+        }
+    }
+    return jsonify(d)
+
+
+@blueprint.route('/getProjectDetailById', methods = ["POST"])
 def getProjectDetailById():
     data = request.get_json()
     projectId = data.get('projectId')
