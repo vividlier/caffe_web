1d686869b7d194c4b558ad10110cfd6aa1915e6a
xiao <2379259142@qq.com>
2017-10-31 16:02:44 +0800

    createDataset接口

--- caffe_web/dataset/routes.py
+++ caffe_web/dataset/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from caffe_web.model import *
 import  json
+from caffe_web.utils import send
 
 blueprint = Blueprint(__name__, __name__)
 
@@ -13,9 +14,27 @@ def test():
     return '{} is ok'.format(b.get("username"))
 
 
-
-
 # @blueprint.route('/test1', methods=['POST'])
 # def test1():
 #     rst = User.query.all()
 #     return rst
+
+
+@blueprint.route('/createDataset', methods=['POST'])
+def createDataset():
+    data = request.get_json()
+    dataset = Dataset(
+        dataset_name=data.get("dataset_name"),
+        dataset_size=data.get("dataset_size"),
+        format=data.get("format"),
+        user_id=data.get("user_id"),
+        path=data.get("path"),
+        description=data.get("description"),
+    )
+    db.session.add(dataset)
+    try:
+        db.session.commit()
+        return send('ok', dataset)
+    except IndexError:
+        db.session.rollback()
+        return send('error', 'error')
