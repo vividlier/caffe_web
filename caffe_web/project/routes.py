a06c78b4dbdbc60d0c7202b3a7e964817d5ee3ba
xiao <2379259142@qq.com>
2017-11-01 15:54:14 +0800

    api

--- caffe_web/project/routes.py
+++ caffe_web/project/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from caffe_web.model import *
+from caffe_web.utils import send
 
 blueprint = Blueprint(__name__, __name__)
 
@@ -8,5 +9,9 @@ def getProjectDetailById():
     data = request.get_json()
     projectId = data.get('projectId')
     project = Project.query.filter(Project.id == projectId).first()
-    print project.project_name
-    return 'detail'
+    tmp = {}
+    tmp["dataset_name"] = project.dataset.dataset_name
+    tmp["project_name"] = project.project_name
+    tmp["net_name"] = project.net.net_name
+    tmp["solver_name"] = project.solver.solver_name
+    return send("ok", tmp)
